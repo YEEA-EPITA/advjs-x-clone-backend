d4f89eaf90aa9305c7f2e2b6031e71e8f78acd0a
Muhammad Arbaz <arbazdogar20@gmail.com>
2025-07-31 14:06:31 +0200

    Add Swagger documentation for the vote on a poll option endpoint

--- routes/poll.routes.js
+++ routes/poll.routes.js
@@ -4,7 +4,28 @@ const { pollController } = require("../controllers");
 const { validateSchemaMiddleware, authMiddleware } = require("../middlewares");
 const { pollSchema } = require("../schemas");
 
-// Vote on a poll option
+/**
+ * @swagger
+ * /polls/vote:
+ *   post:
+ *     summary: Vote on a poll option
+ *     tags: [Polls]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/VotePollInput'
+ *     responses:
+ *       200:
+ *         description: Vote recorded successfully
+ *       403:
+ *         description: Already voted
+ *       500:
+ *         description: Failed to record vote
+ */
 router.post(
   "/vote",
   authMiddleware,
